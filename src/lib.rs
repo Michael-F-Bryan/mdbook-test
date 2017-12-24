2fb303e2a72d875f81e5c7983ffbb8aaf9c03dd3
Michael Bryan <michaelfbryan@gmail.com>
2017-12-24 20:21:28 +0800

    Started figuring out how we'll execute the tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate mdbook;
 extern crate failure;
 
-use failure::error::Error;
+use failure::{ResultExt, Error, SyncFailure};
+use mdbook::renderer::RenderContext;
 
 
+/// Test each of the Rust snippets in a book.
+/// 
+/// This works by:
+/// 
+/// - Creating a new crate in a temporary directory
+/// - Copying across the book chapters as `*.md` files
+/// - Update `Cargo.toml` to include any extra dependencies the user specifies
+/// - Run [rust-skeptic] over the generated crate
+/// 
+/// [rust-skeptic]: https://github.com/budziq/rust-skeptic
 pub fn test(ctx: &RenderContext) -> Result<(), Error> {
+    let cfg: Config = ctx.config.get_deserialized("output.test")
+        .map_err(SyncFailure::new)?;
+
     unimplemented!()
+}
+
+
+/// The configuration struct loaded from the `output.test` table.
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct Config {
+    pub dependencies: Vec<String>,
 }
